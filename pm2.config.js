97c8da8ed8c1a771359dc9e5e635eddffdd5ad29
Crystal <crystalray@qq.com>
2025-10-24 17:24:39 +0800

    1

--- pm2.config.js
+++ pm2.config.js
@@ -8,7 +8,7 @@ function createConfig(name, script) {
             TZ: 'Asia/Shanghai',
             TS_NODE_BASEURL: './dist',
         },
-        log_date_format: 'YYYY-MM-DD HH:mm:ssZ',
+        log_date_format: 'YYYY-MM-DDTHH:mm:ss',
         merge_logs: true,
         error_file: `./runtime/logs/${name}.log`,
         out_file: `./runtime/logs/${name}.log`,
