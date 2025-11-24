5bdc6ad453c31dc63d7effb5de654fffb61b16a6
Crystal <crystalray@qq.com>
2025-11-24 13:07:17 +0800

    1

--- pm2.config.js
+++ pm2.config.js
@@ -19,7 +19,7 @@ function createConfig(name, script, envs = {}) {
 
 module.exports = {
     'crown-matches': createConfig('crown-matches'),
-    // 'crown-matches-data': createConfig('crown-matches-data'),
+    'crown-matches-data': createConfig('crown-matches-data'),
     'crown-robot': createConfig('crown-robot'),
     'final-check': createConfig('final-check'),
     'ready-check': createConfig('ready-check'),
