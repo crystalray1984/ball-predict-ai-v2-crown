24c687fe23ec5fdbec7cec07f52cb3b57144a4f8
Crystal <crystalray@qq.com>
2026-04-10 16:15:47 +0800

    1

---
+++ src/init-team-info.ts
@@ -0,0 +1,52 @@
+import { Match, MatchTeamInfo } from '@/db'
+import { getTeamInfo } from '@/db/models/Match'
+import { Op } from 'sequelize'
+
+async function main() {
+    //计算比赛对阵双方数据
+    let lastMatchId = 0
+    while (true) {
+        const matches = await Match.findAll({
+            where: {
+                id: {
+                    [Op.gt]: lastMatchId,
+                },
+            },
+            order: [['id', 'ASC']],
+            limit: 200,
+            attributes: ['id', 'match_time', 'team1_id', 'team2_id'],
+        })
+
+        if (matches.length === 0) {
+            break
+        }
+
+        for (const match of matches) {
+            lastMatchId = match.id
+            const team1_info = await getTeamInfo(match.team1_id, match.match_time.valueOf())
+            const team2_info = await getTeamInfo(match.team2_id, match.match_time.valueOf())
+
+            const data = {
+                match_id: match.id,
+                team1_goals_scored: team1_info.goals_scored,
+                team1_goals_allowed: team1_info.goals_allowed,
+                team1_matches: team1_info.matches,
+                team2_goals_scored: team2_info.goals_scored,
+                team2_goals_allowed: team2_info.goals_allowed,
+                team2_matches: team2_info.matches,
+            }
+
+            //更新实力数据
+            await MatchTeamInfo.upsert(data, { returning: false })
+
+            console.log(data)
+        }
+    }
+}
+
+main()
+    .then(() => process.exit())
+    .catch((err) => {
+        console.error(err)
+        process.exit()
+    })
